cf2f0a00e6ea1fce8f26d053676b9aa01154d25d
Dominique Parisot <powimod@gmail.com>
2024-02-16 07:26:21 +0100

    Add companyId and field properties in user-object-def

--- src/objects/user-object-def.cjs
+++ src/objects/user-object-def.cjs
@@ -17,79 +17,90 @@
 'use strict'
 
 
-const userObjectDef = { 
+const userObjectDef = {
 	"id" : {
 		"type": "id",
 		"mandatory": "true",
-	}, 
+	},
 	"email" : {
 		"type": "email",
 		"minimum": "3",
 		"maximum": "128",
 		"mandatory": "true",
-	}, 
+	},
 	"password" : {
 		"type": "string",
 		"minimum": "8",
 		"maximum": "70",
 		"mandatory": "true",
 		"secret": "true",
-	}, 
+	},
 	"firstname" : {
 		"type": "string",
 		"maximum": "30",
 		"mandatory": "true",
-	}, 
+	},
 	"lastname" : {
 		"type": "string",
 		"maximum": "30",
 		"mandatory": "true",
-	}, 
+	},
 	"accountLocked" : {
 		"type": "boolean",
+		"field": "account_locked",
 		"default": "false",
 		"mandatory": "true",
-	}, 
+	},
 	"validationCode" : {
 		"type": "integer",
+		"field": "validation_code",
 		"minimum": "10000",
 		"maximum": "99999",
 		"default": "10000",
 		"mandatory": "false",
-	}, 
+	},
 	"phone" : {
 		"type": "string",
 		"maximum": "25",
 		"default": "",
 		"mandatory": "false",
-	}, 
+	},
 	"active" : {
 		"type": "boolean",
 		"default": "true",
 		"mandatory": "true",
-	}, 
+	},
 	"lastUse" : {
 		"type": "datetime",
+		"field": "last_use",
 		"mandatory": "false",
-	}, 
+	},
 	"administrator" : {
 		"type": "boolean",
 		"default": "false",
 		"mandatory": "true",
-	}, 
+	},
 	"stockRole" : {
 		"type": "integer",
+		"field": "stock_role",
+		"minimum": "0",
+		"maximum": "4",
+		"default": "0",
+		"mandatory": "false",
+	},
+	"parkRole" : {
+		"type": "integer",
+		"field": "park_role",
 		"minimum": "0",
 		"maximum": "4",
 		"default": "0",
 		"mandatory": "false",
 	}, 
-	"parkRole" : {
-		"type": "integer",
-		"minimum": "0",
-		"maximum": "4",
-		"default": "0",
-		"mandatory": "false",
+	"companyId" : {
+		"type": "link",
+		"target" : "Company",
+		"field" : "id_company",
+		"table" : "companies"
 	},
 }
 
